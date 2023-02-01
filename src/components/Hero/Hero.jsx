8a2d51eed5fa9799faa797fdca08e43854dfe7e4
shamilkotta <shamilkotta99@gmail.com>
2023-02-01 16:46:47 +0530

    feat: hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import heroImg from "../../assets/img/hero-img.jpg";
+
+function Hero() {
+  return (
+    <div className="h-[75vh] flex flex-col sm:flex-row justify-between mx-auto max-w-[1500px] px-10 ">
+      <div className="w-full sm:w-1/2 self-start sm:self-center order-2 sm:order-1">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-black">
+          Lorem ipsum dolor sit amet, consectetur adipiscing{" "}
+        </h1>
+        <p className="text-base md:text-lg lg:text-xl font-normal my-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor in
+        </p>
+        <button
+          type="button"
+          className="bg-primary px-4 py-2 text-sm md:text-base lg:text-lg font-semibold rounded text-white hover:bg-orange-700"
+        >
+          Get started
+        </button>
+      </div>
+      <div
+        className="w-full sm:w-1/2 h-full bg-center bg-no-repeat bg-contain self-end sm:self-center order-1 sm:order-2"
+        style={{ backgroundImage: `url('${heroImg}')` }}
+      />
+    </div>
+  );
+}
+
+export default Hero;

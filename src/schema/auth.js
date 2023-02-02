c69c5ea384f41338ad6ac1a07308d9854a2978e3
shamilkotta <shamilkotta99@gmail.com>
2023-02-02 13:31:54 +0530

    feat: login and signup validation

---
+++ src/schema/auth.js
@@ -0,0 +1,42 @@
+import * as yup from "yup";
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .trim()
+    .required("Email can not be empty")
+    .email("Enter a valid email"),
+  password: yup
+    .string()
+    .trim()
+    .required("Password can not be empty")
+    .min(8, "Too short password")
+    .max(16, "Too long password"),
+});
+
+export const signupSchema = yup.object().shape({
+  name: yup
+    .string()
+    .transform((value) =>
+      value !== null ? value.charAt(0).toUpperCase() + value.slice(1) : value
+    )
+    .trim()
+    .required("Name can not be empty")
+    .test("isPerfectString", "Enter a valid name", (arg) =>
+      /^[A-Za-z ]+$/.test(arg)
+    ),
+  email: yup
+    .string()
+    .trim()
+    .required("Email can not be empty")
+    .email("Enter a valid email"),
+  password: yup
+    .string()
+    .trim()
+    .required("Password can not be empty")
+    .min(8, "Too short password")
+    .max(16, "Too long password")
+    .test("isPerfectPasswrod", "Enter a strong password", (arg) =>
+      /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])(?!.*\s).{8,16})/.test(arg)
+    ),
+});

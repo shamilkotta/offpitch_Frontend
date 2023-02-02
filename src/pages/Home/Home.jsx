64c0ce2becb28859f1d8eb921147e21a0ead90d6
shamilkotta <shamilkotta99@gmail.com>
2023-02-02 10:12:08 +0530

    feat: footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TournamentCard from "../../components/Cards/TournamentCard";
 import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 import arrowIcon from "../../assets/icons/arrow.svg";
 import leageImg from "../../assets/img/soccor1.jpg";
@@ -10,7 +11,7 @@ import matchImg from "../../assets/img/soccor.jpg";
 import liveImg from "../../assets/img/soccor4.jpeg";
 import linerIcon from "../../assets/icons/liner.svg";
 
-import heroImg from "../../assets/img/hero-img.jpg";
+import ctaImg from "../../assets/img/cta.svg";
 
 function Home() {
   return (
@@ -20,6 +21,7 @@ function Home() {
       <UpcomingSection />
       <QuickButtons />
       <CtaOrganize />
+      <Footer />
     </div>
   );
 }
@@ -93,36 +95,38 @@ function CtaOrganize() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing ",
   ];
   return (
-    <div className="h-[75vh] max-h-[700px] mb-10 sm:max-h-[2000] bg-[#0D084D] text-white flex flex-col sm:flex-row justify-between mx-auto max-w-[1500px] py-6 px-5 sm:px-10 ">
-      <div className="w-full h-full sm:w-1/2 flex flex-col justify-center self-start sm:self-center order-2 sm:order-1 sm:pr-10">
-        <h1 className="text-xl md:text-2xl lg:text-3xl font-black">
-          A place to manage your next match
-        </h1>
-        <p className="text-sm md:text-base font-normal my-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor
-        </p>
-        <ul className="my-4">
-          {cta.map((ele) => (
-            <li className="my-2">
-              <p className="flex gap-x-4">
-                <img src={linerIcon} alt="liner" />
-                {ele}
-              </p>
-            </li>
-          ))}
-        </ul>
-        <button
-          type="button"
-          className="bg-primary w-fit px-4 py-2 text-sm md:text-base lg:text-lg font-semibold rounded text-white hover:bg-orange-700"
-        >
-          Host a match
-        </button>
+    <div className="bg-[#0D084D]">
+      <div className="h-fit max-h-[700px] mb-10 sm:max-h-[2000] text-white flex flex-col sm:flex-row justify-between mx-auto max-w-[1500px] py-20 px-5 sm:px-10 ">
+        <div className="w-full h-full sm:w-1/2 flex flex-col justify-center self-start sm:self-center order-2 sm:order-1 sm:pr-10">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-black">
+            A place to manage your next match
+          </h1>
+          <p className="text-sm md:text-base font-normal my-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor
+          </p>
+          <ul className="my-4">
+            {cta.map((ele) => (
+              <li className="my-2">
+                <p className="flex gap-x-4">
+                  <img src={linerIcon} alt="liner" />
+                  {ele}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="bg-primary w-fit px-4 py-2 text-sm md:text-base lg:text-lg font-semibold rounded text-white hover:bg-orange-700"
+          >
+            Host a match
+          </button>
+        </div>
+        <div
+          className="w-full mb-5 sm:w-1/2 h-96 bg-center bg-no-repeat bg-contain self-end sm:self-center order-1 sm:order-2"
+          style={{ backgroundImage: `url('${ctaImg}')` }}
+        />
       </div>
-      <div
-        className="w-full sm:w-1/2 h-full bg-center bg-no-repeat bg-contain self-end sm:self-center order-1 sm:order-2"
-        style={{ backgroundImage: `url('${heroImg}')` }}
-      />
     </div>
   );
 }

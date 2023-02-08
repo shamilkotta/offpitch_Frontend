ae7b143b750ffba8de25462b5f996ea3ccbc4c87
shamilkotta <shamilkotta99@gmail.com>
2023-02-08 07:46:47 +0530

    feat: persist login state on refresh

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -7,24 +7,26 @@ import OtpConfirmation from "./OtpConfirmation/OtpConfirmation";
 import Signup from "./Signup/Signup";
 import Notfound from "./Notfound";
 import UserAuthorization from "../components/Authorization/UserAuthorization";
+import PersistLogin from "../components/PersistLogin";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<UserLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/verify-email" element={<OtpConfirmation />} />
+      <Route element={<PersistLogin />}>
+        <Route path="/" element={<UserLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/verify-email" element={<OtpConfirmation />} />
+          <Route element={<UserAuthorization />}>
+            <Route path="/profile" />
+          </Route>
 
-        <Route element={<UserAuthorization />}>
-          <Route path="/profile" />
+          <Route path="/admin" />
+
+          {/* 404 */}
+          <Route path="*" element={<Notfound />} />
         </Route>
-
-        <Route path="/admin" />
-
-        {/* 404 */}
-        <Route path="*" element={<Notfound />} />
       </Route>
       <Route path="/admin" />
     </Routes>

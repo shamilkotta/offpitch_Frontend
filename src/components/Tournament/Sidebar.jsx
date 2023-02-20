4b4cb905139b0c73a32c62d3aec5101fc30a3a50
shamilkotta <shamilkotta99@gmail.com>
2023-02-20 21:41:38 +0530

    refactor: file rename

--- src/components/Tournament/Sidebar.jsx
+++ src/components/Tournament/Sidebar.jsx
@@ -1,25 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import calendarIcon from "../../assets/icons/calendar-tick-dark.svg";
 import locationIcon from "../../assets/icons/location-dark.svg";
 import ticketIcon from "../../assets/icons/ticket.svg";
 import calendarIconLight from "../../assets/icons/calendar-tick.svg";
 import location from "../../assets/icons/location.svg";
+import clockIcon from "../../assets/icons/clock.svg";
 import { InputSubmit } from "../InputFields/InputFields";
 
-function Sidbar() {
+function Sidebar({ data }) {
   return (
-    <div className="mx-auto md:px-3 md:max-w-[450px] my-6">
-      <div className="mt-2 mb-6">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
-          libero provident, minus facere nihil molestiae laudantium corrupti sit
-          iure dolor dolores, doloribus incidunt, cupiditate dolore? Facilis a
-          dolorum quas minus. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Pariatur quisquam quibusdam beatae, et accusantium tempore
-          explicabo eveniet perspiciatis dolorem omnis recusandae, ipsa vel
-          illo. Quae, incidunt. Sunt ipsa totam corporis.
-        </p>
+    <div className="mx-auto md:px-3 md:max-w-[450px] mb-6">
+      <div className="mb-6">
+        <p>{data.short_description}</p>
       </div>
       <div className="flex justify-start gap-x-5 gap-y-3 flex-wrap my-6">
         <div className="flex gap-1">
@@ -34,8 +28,8 @@ function Sidbar() {
             />
           </button>
           <div className="whitespace-nowrap text-sm">
-            <p>12PM</p>
-            <p>23, January 2023</p>
+            <p>{data.time}</p>
+            <p>{data.date}</p>
           </div>
         </div>
         <div className="flex gap-1">
@@ -46,27 +40,30 @@ function Sidbar() {
             <img src={location} alt="date" className="mx-auto h-full w-full" />
           </button>
           <div className="whitespace-nowrap  text-sm">
-            <p>Calicut</p>
-            <p>Kerala, Ind</p>
+            <p>{data.place}</p>
+            <p>
+              {data.state}, {data.country}
+            </p>
           </div>
         </div>
       </div>
+
       <div className="my-10">
         <p className="text-xl font-semibold mb-3">Buy tickets</p>
         <div className="border rounded-lg py-2 px-3">
-          <p className="text-base text-gray-600">City club</p>
-          <p className="text-lg -mt-1 mb-4 font-medium">
-            City premier league by city org
-          </p>
+          <p className="text-base text-gray-600">{data.host.name}</p>
+          <p className="text-lg -mt-1 mb-4 font-medium">{data.title}</p>
           <div className="flex flex-wrap gap-x-8 gap-y-4">
             <div className="flex flex-col gap-y-1">
               <div className="flex items-center gap-1">
                 <img src={calendarIcon} alt="calendar" />
-                <p className="text-sm">23, january 2023</p>
+                <p className="text-sm">{data.date}</p>
               </div>
               <div className="flex items-center gap-1">
                 <img src={locationIcon} alt="calendar" />
-                <p className="text-sm">Kerala, Ind</p>
+                <p className="text-sm">
+                  {data.state}, {data.country}
+                </p>
               </div>
             </div>
             <div>
@@ -80,18 +77,61 @@ function Sidbar() {
           <div className="flex justify-between">
             <div>
               <p className="">
-                ₹<span className="text-3xl font-semibold">999</span>
+                ₹
+                <span className="text-3xl font-semibold">
+                  {data.ticketPrice}
+                </span>
               </p>
             </div>
-            <InputSubmit type="button" value="Buy now" className="w-1/2" />
+            <InputSubmit
+              type="button"
+              value="Buy now"
+              className="w-1/3 min-w-[100px]"
+            />
           </div>
         </div>
       </div>
       <div className="my-10">
-        <p className="text-xl font-semibold mb-2">Register your team</p>
+        <p className="text-xl font-semibold mb-3">Register you team</p>
+        <div className="rounded-lg py-2 px-3">
+          <div className="flex justify-between flex-wrap  gap-x-8 gap-y-4">
+            <div className="flex flex-col gap-y-1">
+              <div className="flex items-start gap-1">
+                <img src={clockIcon} alt="last date" className="self-start" />
+                <div className="">
+                  <p className="text-sm">Last date: </p>
+                  <p className="text-sm">{data.lastDate}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center gap-1">
+                <img src={ticketIcon} alt="calendar" className="w-4" />
+                <p className="text-sm">Fee:</p>
+              </div>
+              <p className="">
+                ₹
+                <span className="text-3xl font-semibold">
+                  {data.registrationPrice}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-4  justify-end">
+            <InputSubmit
+              type="button"
+              value="Register now"
+              className="w-1/3 min-w-[130px]"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Sidbar;
+Sidebar.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Sidebar;

34f36fd5bede2f8729851bcebdbd0a4f87076c70
shamilkotta <shamilkotta99@gmail.com>
2023-02-20 18:18:36 +0530

    feat: add dynamic data loading

--- src/components/Tournament/About.jsx
+++ src/components/Tournament/About.jsx
@@ -1,39 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import soccorImg from "../../assets/img/soccor3.jpg";
 
-function About() {
+function About({ data }) {
   return (
-    <div className="my-10">
+    <div className="mb-10 mt-6">
       <p className="text-lg font-semibold">About</p>
-      <div className="text-gray-600">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus rem
-        nostrum officiis? Error enim eius possimus, deleniti fugit quaerat
-        repellat accusantium commodi eum. Enim, architecto. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Ipsam adipisci, iure voluptas
-        odit optio commodi placeat harum. Animi repellendus deleniti iusto
-        voluptates, consequatur nesciunt laborum. Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Hic necessitatibus amet reprehenderit
-        magnam quam voluptas autem fugit nemo ad libero excepturi enim, dolor
-        qui rem obcaecati inventore facere iste velit, assumenda, ducimus odio
-        sed ratione eveniet ipsa? Autem, quos!
-      </div>
+      <div className="text-gray-600">{data.description}</div>
       <div className="flex flex-wrap items-center gap-y-3 gap-x-8 my-8">
         <div className="flex items-center">
           <img className="w-12 h-12 rounded-full mr-2" src={soccorImg} alt="" />
           <div className="self-end">
             <p className="font-medium m-0 text-gray-500">Organized by</p>
-            <p className="text-lg font-semibold -mt-2">City club</p>
+            <p className="text-lg font-semibold -mt-2">{data.host.name}</p>
           </div>
         </div>
         <div className="border-l-2 h-10" />
         <div className="flex flex-col justify-end">
-          <p className="-mt-1">9189765487</p>
-          <p className="-mt-1">hello@city.com</p>
+          <p className="-mt-1">{data.host.phone}</p>
+          <p className="-mt-1">{data.host.email}</p>
         </div>
       </div>
     </div>
   );
 }
+
+About.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default About;
--- src/components/Tournament/Header.jsx
+++ src/components/Tournament/Header.jsx
@@ -1,21 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import soccorImg from "../../assets/img/soccor2.jpg";
 import "./Tournament.scss";
 
-function Header() {
+function Header({ data }) {
   return (
-    <div className="flex-col">
-      <p className="text-3xl font-bold mb-5">
-        Lorem ipsum dolor lorem ipsum dolor sit. lorem ipsum dolor sit. sit
-        amet.
-      </p>
-      <div
-        style={{ backgroundImage: `url('${soccorImg}')` }}
-        className="rounded w-full aspect-[8/5] bg-cover bg-center bg-no-repeat col-start-1 col-end-2 "
-      />
-    </div>
+    <div
+      style={{ backgroundImage: `url('${data.cover}')` }}
+      className="rounded w-full aspect-[16/9] bg-cover bg-center bg-no-repeat"
+    />
   );
 }
+
+Header.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default Header;

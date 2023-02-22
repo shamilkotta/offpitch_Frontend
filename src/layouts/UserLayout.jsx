a00035a65d85101cf51c2a088b6848b25d034fea
shamilkotta <shamilkotta99@gmail.com>
2023-02-22 12:39:17 +0530

    refactor: change file name of menu bar

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/MenuBar/Navbar";
 
 function UserLayout() {
   return (

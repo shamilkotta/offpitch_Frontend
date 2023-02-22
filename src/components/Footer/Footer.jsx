d71da6c88e66b93ba6dfbbcd0f82f4f1f34aa90b
shamilkotta <shamilkotta99@gmail.com>
2023-02-22 12:11:36 +0530

    refactor: update ui styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,14 @@ function Footer() {
       <div className="max-w-[1500px] mx-auto box-border">
         <div className="py-10 px-5 sm:p-10 flex flex-wrap gap-4 justify-between items-center">
           <div>
-            <h1 className="text-3xl font-bold">offpitch</h1>
-            <p className="max-w-[300px]">
+            <h1 className="text-3xl font-bold text-black/80">offpitch</h1>
+            <p className="max-w-[300px] text-gray-500">
               Our motto to fulfill customer demand by making them satisfied with
               growing their business.
             </p>
           </div>
           <div>
-            <h1 className="text-lg font-bold">About</h1>
+            <h1 className="text-lg font-bold text-black/80">About</h1>
             <ul className="ml-1">
               <li className="my-2 text-base font-light text-gray-500 hover:text-gray-600 cursor-pointer">
                 About Us
@@ -30,7 +30,7 @@ function Footer() {
             </ul>
           </div>
           <div>
-            <h1 className="text-lg font-bold">Support</h1>
+            <h1 className="text-lg font-bold text-black/80">Support</h1>
             <ul className="ml-1">
               <li className="my-2 text-base font-light text-gray-500 hover:text-gray-600 cursor-pointer">
                 Account
@@ -47,7 +47,7 @@ function Footer() {
             </ul>
           </div>
           <div>
-            <h1 className="text-lg font-bold">Get our app</h1>
+            <h1 className="text-lg font-bold text-black/80">Get our app</h1>
             <ul className="ml-1">
               <li className="my-2 text-base font-light text-gray-500 hover:text-gray-600 cursor-pointer">
                 Account

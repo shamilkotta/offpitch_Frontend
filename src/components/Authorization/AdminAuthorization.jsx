6ea08a3c442f45a0b6ccfe3c6b4fc49a3cb38cc3
shamilkotta <shamilkotta99@gmail.com>
2023-02-22 12:36:18 +0530

    feat: create admin role based authorization

---
+++ src/components/Authorization/AdminAuthorization.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-nested-ternary */
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
+
+function AdminAuthorization() {
+  const auth = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  return auth?.accessToken && auth?.role === "admin" ? (
+    <Outlet />
+  ) : auth?.accessToken ? (
+    <Navigate to="/not-found" replace />
+  ) : (
+    <Navigate to="/login" state={{ from: location.pathname }} replace />
+  );
+}
+
+export default AdminAuthorization;

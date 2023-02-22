8ab7bb29108ae72973a2bbd87ad4ed5f0d8e91f8
shamilkotta <shamilkotta99@gmail.com>
2023-02-22 12:35:36 +0530

    feat: user role based authorization

--- src/components/Authorization/UserAuthorization.jsx
+++ src/components/Authorization/UserAuthorization.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useLocation, Navigate } from "react-router-dom";
@@ -6,8 +7,10 @@ function UserAuthorization() {
   const auth = useSelector((state) => state.auth);
   const location = useLocation();
 
-  return auth?.accessToken ? (
+  return auth?.accessToken && auth?.role !== "admin" ? (
     <Outlet />
+  ) : auth?.accessToken && auth?.role === "admin" ? (
+    <Navigate to="/admin" state={{ from: location.pathname }} replace />
   ) : (
     <Navigate to="/login" state={{ from: location.pathname }} replace />
   );

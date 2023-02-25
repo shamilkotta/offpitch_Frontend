b43a2a8c34d43f1bfb497fd909ffb2c89da07ea2
shamilkotta <shamilkotta99@gmail.com>
2023-02-25 20:32:39 +0530

    feat: admin routes added

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import UserLayout from "../layouts/UserLayout";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
@@ -8,11 +9,13 @@ import Signup from "./Signup/Signup";
 import Notfound from "./Notfound";
 import UserAuthorization from "../components/Authorization/UserAuthorization";
 import PersistLogin from "../components/PersistLogin";
-import MyOrganization from "./Organization/MyOrganization";
 import MyClub from "./Club/MyClub";
 import NewTournament from "./Tournament/NewTournament";
 import EditTournament from "./Tournament/EditTournament";
 import ResetPassword from "./ResetPassword/ResetPassword";
+import AdminAuthorization from "../components/Authorization/AdminAuthorization";
+import Clubs from "./Admin/Clubs";
+import AdminLayout from "../layouts/AdminLayout";
 
 function Router() {
   return (
@@ -24,9 +27,10 @@ function Router() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/verify-email" element={<OtpConfirmation />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+          {/* user account */}
           <Route path="/user" element={<UserAuthorization />}>
-            <Route path="/user/profile" />
-            <Route path="/user/organization" element={<MyOrganization />} />
+            <Route path="/user" />
             <Route path="/user/club" element={<MyClub />} />
             <Route path="/user/tournament/new" element={<NewTournament />} />
             <Route
@@ -34,14 +38,20 @@ function Router() {
               element={<EditTournament />}
             />
           </Route>
+        </Route>
 
-          <Route path="/admin" />
+        {/* admin account */}
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminAuthorization />}>
+            <Route path="/admin/clubs" element={<Clubs />} />
+          </Route>
+        </Route>
 
-          {/* 404 */}
+        {/* 404 */}
+        <Route element={<UserLayout />}>
           <Route path="*" element={<Notfound />} />
         </Route>
       </Route>
-      <Route path="/admin" />
     </Routes>
   );
 }

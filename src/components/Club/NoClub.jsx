99e3eb111c63c57c783de1c3867f4f407a71e551
shamilkotta <shamilkotta99@gmail.com>
2023-03-13 16:23:12 +0530

    refactor: update ui for club status

--- src/components/Club/NoClub.jsx
+++ src/components/Club/NoClub.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 
 import useRefreshToken from "../../hooks/useRefreshToken";
 import ClubForm from "./ClubForm";
+import noClubImg from "../../assets/img/no-club.svg";
+import waitingImg from "../../assets/img/waiting.svg";
+import rejectImg from "../../assets/img/reject.svg";
+import blockImg from "../../assets/img/block.svg";
 
 function NoClub({ status }) {
   const [showForm, setShowForm] = useState(false);
@@ -11,8 +15,9 @@ function NoClub({ status }) {
 
   if (clubStatus === "blocked")
     return (
-      <div className="h-[80vh] flex justify-center ">
-        <div className="mt-10 shadow-sm border h-fit py-6 px-5 w-11/12 sm:w-4/5 md:w-1/2 max-w-[900px] flex flex-wrap justify-center items-center gap-x-2 rounded text-center box-border">
+      <div className="h-[80vh] flex justify-center flex-col items-center">
+        <img src={blockImg} className="w-80" alt="no club" />
+        <div className="py-6 px-5 flex flex-wrap justify-center items-center gap-x-2 text-center">
           <h2 className="text-lg">Your club is blocked!</h2>
           <button type="button" className="text-primary text-lg">
             Contact support center
@@ -23,8 +28,9 @@ function NoClub({ status }) {
 
   if (clubStatus === "awaiting")
     return (
-      <div className="h-[80vh] flex justify-center ">
-        <div className="mt-10 shadow-sm border h-fit py-6 px-5 w-11/12 sm:w-4/5 md:w-1/2 max-w-[900px] flex flex-wrap justify-center items-center gap-x-2 rounded text-center box-border">
+      <div className="h-[80vh] flex justify-center flex-col items-center">
+        <img src={waitingImg} className="w-80" alt="waiting for approval" />
+        <div className="py-6 px-5 flex flex-wrap justify-center items-center gap-x-2 text-center">
           <h2 className="text-lg">Your club is waiting for approval,</h2>
           <h2 className="text-lg">Check back later!</h2>
         </div>
@@ -33,8 +39,9 @@ function NoClub({ status }) {
 
   if (clubStatus === "rejected")
     return (
-      <div className="h-[80vh] flex justify-center ">
-        <div className="mt-10 shadow-sm border h-fit py-6 px-5 w-11/12 sm:w-4/5 md:w-1/2 max-w-[900px] flex flex-wrap justify-center items-center gap-x-2 rounded text-center box-border">
+      <div className="h-[80vh] flex justify-center flex-col items-center">
+        <img src={rejectImg} className="h-72" alt="no club" />
+        <div className="py-6 px-5 flex flex-wrap justify-center items-center gap-x-2 text-center">
           <h2 className="text-lg">New club request rejected,</h2>
           <button type="button" className="text-primary text-lg">
             Contact support center
@@ -63,8 +70,9 @@ function NoClub({ status }) {
     );
 
   return (
-    <div className="h-[80vh] flex justify-center ">
-      <div className="mt-10 shadow-sm border h-fit py-6 px-5 w-11/12 sm:w-4/5 md:w-1/2 max-w-[900px] flex flex-wrap justify-center items-center gap-x-2 rounded text-center box-border">
+    <div className="h-[80vh] flex justify-center items-center flex-col">
+      <img src={noClubImg} className="w-80" alt="no club" />
+      <div className="py-6 px-5  flex flex-wrap justify-center items-center gap-x-2 text-center">
         <h2 className="text-lg">You don&apos;t have a club!</h2>
         <button
           type="button"

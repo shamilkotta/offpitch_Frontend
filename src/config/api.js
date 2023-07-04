a44b08e25f2548c8c60676094d32ecd355442816
shamilkotta <shamilkotta99@gmail.com>
2023-07-04 09:03:14 +0530

    refactor: change base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 
 const axiosPublic = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
